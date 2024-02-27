8fb61d37734e7e8217cf219a6011816d42e1a45f
noahsadir <development.noahsadir@gmail.com>
2024-02-27 11:41:57 -0500

    Add readme and modify instructions

--- ProtocolsErrors.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ ProtocolsErrors.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@ import Foundation
  */
 
 
-// Basic Protocol
+// [1] Basic Protocol
 
 /**
  Below is a basic protocol modeling a generic personal id card. We use the protocol keyword to declare the protocol.
@@ -46,47 +46,60 @@ is left up to the struct/class implementing it.
  Below, you will implement the PersonalID protocol to create a Driver's License object. Follow the steps listed to assist you.
  */
 
-// TASK 1
+// [2] Implement DriversLicense which conforms to PersonalID and Identifiable
 
 struct DriversLicense: PersonalID {
     /**
         Steps To Do:
-            1. Implement the protocol requirements specified in PersonalID. The function implementation is up to your interpretation.
-            2. Add 3 additional properties and 1 function that apply solely to drivers licenses.
+            1. Implement the protocol requirements specified in PersonalID.
+              The function implementation is up to your interpretation.
+            2. Add 3 additional properties and 1 function that apply solely to
+              drivers licenses.
             3. Make DriversLicense also conform to Identifiable.
      */
 }
 
 /**
- Now that you have implemented a basic protocol, you can see how protocols add structure to your code and make it easier to keep track of models.
+ Now that you have implemented a basic protocol, you can see how protocols add
+ structure to your code and make it easier to keep track of models.
  
  Let's create an instance of your object and test the functionality.
  */
 
-// TASK 2
+// [3] Test out the functionality of your newly created struct
 
-var myDriversLicense: DriversLicense = DriversLicense() // provide values for your properties as arguments to remove error messages
+// Provide values for your properties as arguments to remove error messages
+//
+// If there are no error messages, you might want to take a second look
+// at your implementation for [2]
+var myDriversLicense: DriversLicense = DriversLicense()
 
 // print your license information on the line below.
 
 // now, change your address and print toString again.
 
 
-// TASK 3
+// [4] Create a Buzzcard struct which conforms to PersonalID and Hashable
 
 /**
- Next, you will create another personal id, Buzzcard, that conforms to our protocol. Follow the same 3 steps as in DriversLicense.
+ Next, you will create another personal id, Buzzcard, that conforms to our
+ protocol. Follow the same 3 steps as in DriversLicense.
+ 
  After that, make Buzzcard hashable.
  */
 
 // ADD YOUR PROTOCOL STARTING BELOW THIS LINE
 
 
-// TASK 4
+// [5] Create an extension of PersonalID which consolidates redundant code.
 
 /**
- Now that you have implemented protocols in multiple structs, you should have noticed some redundancies in your code, such as the "move" function.
- If you predict that a function will have identical implementations in multiple structs/classes, you can provide a default implementation using a protocol EXTENSION.
+ Now that you have implemented protocols in multiple structs, you should have
+ noticed some redundancies in your code, such as the "move" function.
+ If you predict that a function will have identical implementations in multiple
+ structs/classes, you can provide a default implementation using a protocol
+ EXTENSION.
+ 
  Copy your code for the move function into the extension shell code below.
  */
 
@@ -96,11 +109,13 @@ extension PersonalID {
     }
 }
 
-// TASK 5
+// [6] Implement an extension from scratch which adds toString() functionality
 
 /**
- Once you have implemented a function in a protocol extension, you can choose to delete the implementation in your structs/classes, or leave it if you would like a
- more specific implementation. Below, add a protocol extension for the "toString" method.
+ Once you have implemented a function in a protocol extension, you can choose
+ to delete the redundant function in your structs/classes, or leave it if you
+ would like a more specific implementation. Below, add a protocol extension
+ for the "toString" method.
  */
 
 // your code here
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# bootcamp-module-4
+# Module 4: Protocols, Extensions, and Error Handling
+
+(Derived from [Nick Candello's](https://github.com/nac5504/week_5_bootcamp) repo)
+
+This week, we'll be going over how Swift deals with blueprinting via protocols,
+along with methods for error handling.
+
+#### What you'll accomplish today
+
+- [ ] Implement properties of a protocol
+- [ ] Write a protocol from scratch
+- [ ] Extend functionality of a protocol
+- [ ] Create a custom Error type
+- [ ] Utilize do-catch for error handling
+- [ ] Utilize optional conversion for error handling
+
+## Getting started
+
+Clone this repo onto your local device, either using Xcode or the command line.
+
+Once you've cloned it, open the `ProtocolsErrors` playground file if you
+haven't already.
+
+## Protocols (and Extensions)
+
+In the _Protocols_ page of the playground, follow the instructions provided in
+the file.
+
+1. Take a look at `PersonalID`. Notice that the syntax is familiar to classes
+   and structs, but with the `protocol` keyword used instead. Also notice that
+   variable and function signatures are declared, but not defined.
+   
+2. Now take a look at `DriversLicense`. It is a struct which implements the
+   `PersonalID` protocol. Similar to implementing Java interfaces, classes or
+   structs which implement a protocol _must_ define its functions.
+   
+3. We've now defined a struct which conforms to `PersonalID`. Follow the
+   instructions to properly initialize, manipulate, and display an instance
+   of `DriversLicense`
+   
+4. One of the primary purposes of a protocol is that it can serve as a common
+   definition for multiple types of objects (aka polymorphism). Create a new
+   struct called `Buzzcard` which conforms to `PersonalID`.
+   
+5. You may have noticed that you've had to define the `move` function
+   individually for each struct even though the code is (or should be)
+   identical. Implement the extension provided which changes the ID's address.
+   
+6. Now, try adding a `toString()` extension from scratch. You can refer to the
+   implementation in step 5 if needed.
+   
+#### What you've accomplished so far:
+
+- [x] Implement properties of a protocol
+- [x] Write a protocol from scratch
+- [x] Extend functionality of a protocol
+
+## Error Handling
